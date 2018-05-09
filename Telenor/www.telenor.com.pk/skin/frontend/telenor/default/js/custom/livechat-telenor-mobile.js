fe6818e3a4f213ab2db35db54e45b587d4ae06ab
mbilalmalik <bilal.malik@mercurialminds.com.pk>
2018-05-09 10:14:55 +0500

    Adding clone websites

---
+++ Telenor/www.telenor.com.pk/skin/frontend/telenor/default/js/custom/livechat-telenor-mobile.js
@@ -0,0 +1,28 @@
+var __lc = {};
+__lc.license = 3528751;
+__lc.group = 12;
+//alert("telenor mobile live chat");
+
+// var LC_API = LC_API || {};
+// LC_API.open_chat_window();
+//alert("telenor live chat");
+var LC_API = LC_API || {};
+LC_API.on_after_load = function()
+{
+	$jq(document).ready(function() {
+		$jq('#chat_area').hide();
+	});
+};
+
+
+LC_API.on_before_load = function()
+{
+	LC_API.open_chat_window();
+};
+
+
+(function() {
+ var lc = document.createElement('script'); lc.type = 'text/javascript'; lc.async = true;
+ lc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.livechatinc.com/tracking.js';
+ var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(lc, s);
+})();
